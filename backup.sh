9b2a442e1e02c52a62b8b039b30a34c46936ef15
Simon Haas <me@simonhaas.eu>
2021-06-01 07:34:29 +0000

    added verbose mode

--- backup.sh
+++ backup.sh
@@ -6,17 +6,17 @@ if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/omv' /proc/mounts
 then
     echo "lets backup omv"
 
-    rdiff-backup --remove-older-than 30B /mnt/backup/rdiff-backup/omv
-    rdiff-backup /mnt/omv /mnt/backup/rdiff-backup/omv
+    rdiff-backup --remove-older-than 4W --force /mnt/backup/rdiff-backup/omv
+    rdiff-backup -v5 --print-statistics /mnt/omv /mnt/backup/rdiff-backup/omv
 fi
 
 if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/external4TB' /proc/mounts
 then
     echo "lets backup docker"
 
-    rdiff-backup --remove-older-than 30B /mnt/backup/rdiff-backup/docker
+    rdiff-backup --remove-older-than 4W --force /mnt/backup/rdiff-backup/docker
     
     docker-compose down
-    rdiff-backup /mnt/external4TB/docker /mnt/backup/rdiff-backup/docker
+    rdiff-backup -v5 --print-statistics /mnt/external4TB/docker /mnt/backup/rdiff-backup/docker
     docker-compose up -d
 fi
