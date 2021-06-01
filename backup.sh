b027ff2c86e3d0d71fe7a0b94e79d9739d822190
Simon Haas <me@simonhaas.eu>
2021-06-01 17:30:08 +0000

    fixed path for clarkson

--- backup.sh
+++ backup.sh
@@ -2,11 +2,13 @@
 
 echo "Hello World"
 
+#TODO wenn Ziel noch kein rdiff-backup dir, dann erstmal rsync bis alles einmal kopiert wurde
+
 if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/omv' /proc/mounts
 then
     echo "lets backup omv"
 
-    rdiff-backup --remove-older-than 4W --force /mnt/backup/rdiff-backup/omv
+    rdiff-backup --remove-older-than 4W /mnt/backup/rdiff-backup/omv
     rdiff-backup -v5 --print-statistics /mnt/omv /mnt/backup/rdiff-backup/omv
 fi
 
@@ -14,7 +16,7 @@ if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/external4TB' /proc/moun
 then
     echo "lets backup docker"
 
-    rdiff-backup --remove-older-than 4W --force /mnt/backup/rdiff-backup/docker
+    rdiff-backup --remove-older-than 4W /mnt/backup/rdiff-backup/docker
     
     docker-compose down
     rdiff-backup -v5 --print-statistics /mnt/external4TB/docker /mnt/backup/rdiff-backup/docker
