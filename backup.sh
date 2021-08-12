c1e79d151d47dae331a5ec57aa332f6ac1fef8e0
Simon Haas <me@simonhaas.eu>
2021-08-12 17:18:24 +0200

    backup script more verbose

--- backup.sh
+++ backup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 # sudo crontab -e
-# 0 0 * * *  /mnt/external4TB/docker/backup.sh &>> /mnt/external4TB/docker/backup-logs.txt
+# 0 0 * * *  /mnt/external4TB/docker/backup.sh >> /mnt/external4TB/docker/backup-logs.txt 2>&1
 
+echo "########### START ###########"
 echo `date`
 
 #TODO wenn Ziel noch kein rdiff-backup dir, dann erstmal rsync bis alles einmal kopiert wurde
 
 if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/omv' /proc/mounts
 then
-    echo "lets backup omv"
+    echo "########### lets backup omv"
 
+    #rdiff-backup -l /mnt/backup/rdiff-backup/omv
     rdiff-backup --remove-older-than 4W /mnt/backup/rdiff-backup/omv
     rdiff-backup -v5 --print-statistics /mnt/omv /mnt/backup/rdiff-backup/omv
 fi
 
 if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/external4TB' /proc/mounts
 then
-    echo "lets backup docker"
+    echo "########### lets backup docker"
 
+    #rdiff-backup -l /mnt/backup/rdiff-backup/docker
     rdiff-backup --remove-older-than 4W /mnt/backup/rdiff-backup/docker
     
     ./script.sh stop
     rdiff-backup -v5 --print-statistics /mnt/external4TB/docker /mnt/backup/rdiff-backup/docker
     ./script.sh start
 fi
+
+echo "########### END ###########"
