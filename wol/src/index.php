aab374f094a9c0b27b863df0cf78278127d4958f
Simon Haas <me@simonhaas.eu>
2021-12-27 21:13:47 +0100

    wol script updated

--- wol/src/index.php
+++ wol/src/index.php
@@ -4,7 +4,13 @@ $param = substr($_GET['MAC'], 0, 17);
 $found = preg_match('/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/', $param, $matches);
 
 if (!$found) {
-        echo "no MAC address supplied";
+?>
+        <form action="/" method="GET">
+                <label for="mac">MAC Address</label><br>
+                <input type="text" id="mac" name="MAC" value=""><br>
+                <input type="submit" value="Submit">
+        </form>
+<?php
         die();
 }
 
