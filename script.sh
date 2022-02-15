0cb1b49b58ed71d26aec72028e85f875c09e051d
Simon Haas <me@simonhaas.eu>
2022-02-15 20:14:28 +0100

    custom_services

--- script.sh
+++ script.sh
@@ -11,6 +11,18 @@ IFS=',' read -r -a services <<< "$servicesEnv"
 for service in ${services[*]}
 do
     cd services/$item
+    #TODO cp .env.example .env if no .env exists
+    docker-compose $1 $2 $3
+    cd ../..
+done
+
+customServicesEnv="${SERVICES:-default_value}"
+IFS=',' read -r -a customServices <<< "$customServicesEnv"
+
+for service in ${customServices[*]}
+do
+    cd custom_services/$item
+    #TODO cp .env.example .env if no .env exists
     docker-compose $1 $2 $3
     cd ../..
 done
