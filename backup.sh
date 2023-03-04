d8f23eea6afc94870531c28ac78f11e829a525fc
Simon Haas <me@simonhaas.eu>
2023-03-04 16:24:04 +0100

    backup script in english

--- backup.sh
+++ backup.sh
@@ -1,70 +1,48 @@
 # !/bin/bash
-# Skriptvorlage BorgBackup
-# https://wiki.ubuntuusers.de/BorgBackup/
-# https://borgbackup.readthedocs.io/en/stable/
 
 # sudo crontab -e
 # 0 0 * * * (time /bin/bash /mnt/ssd2/homeserver/backup.sh) >> /mnt/ssd2/homeserver/backup-logs.txt 2>&1
 
-# Hier Pfad zum Sicherungsmedium angeben.
-# z.B. zielpfad="/media/peter/HD_Backup"
-zielpfad="/mnt/backup"
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
 
-# Hier Namen des Repositorys angeben.
-# z.B. repository="borgbackups"
+destination=${BACKUP_DESTINATION}
 repository="homeserver"
-
-# Hier eine Liste mit den zu sichernden Verzeichnissen angeben
-# z.B. sicherung="/home/peter/Bilder /home/peter/Videos --exclude *.tmp"
-sicherung="/mnt/data/homeserver"
-
-# Hier die Art der Verschlüsselung angeben
-# z.B. verschluesselung="none"
-verschluesselung="none"
-
-# Hier die Art der Kompression angeben
-# z.B. kompression="none"
-kompression="lz4"
-
-# Hier angeben, ob vor der Ausführung von BorgBackup auf vorhandene Root-Rechte geprüft werden soll
-# z.B. rootuser="ja"
-rootuser="ja"
-
-# Hier angeben nach welchem Schema alte Archive gelöscht werden sollen.
-# Die Vorgabe behält alle Sicherungen des aktuellen Tages. Zusätzlich das aktuellste Archiv der
-# letzten 7 Sicherungstage, der letzten 4 Wochen sowie der letzten 12 Monate.
+source=$( pwd; )
+encryption="none"
+compression="lz4"
+rootuser="yes"
 pruning="--keep-within=1d --keep-daily=7 --keep-weekly=4 --keep-monthly=12"
 
-###################################################################################################
+repopath="$destination"/"$repository"
 
-repopfad="$zielpfad"/"$repository"
-
-if grep -qs '/mnt/backup' /proc/mounts && grep -qs '/mnt/data' /proc/mounts
+if grep -qs $destination /proc/mounts && grep -qs $source /proc/mounts
 then
-    # check for root
-    if [ $(id -u) -ne 0 ] && [ "$rootuser" == "ja" ]; then
-    echo "Sicherung muss als Root-User ausgeführt werden."
-    exit 1
+
+    if [ $(id -u) -ne 0 ] && [ "$rootuser" == "yes" ]; then
+        echo "Backup has to be run as root user."
+        exit 1
     fi
 
-    # Init borg-repo if absent
-    if [ ! -d $repopfad ]; then
-    borg init --encryption=$verschluesselung $repopfad
-    echo "Borg-Repository erzeugt unter $repopfad"
+    if [ ! -d $repopath ]; then
+        borg init --encryption=$encryption $repopath
+        echo "Borg-repository created at $repopath"
     fi
 
     SECONDS=0
-    /mnt/data/homeserver/script.sh SAVE_BACKUP stop
-    echo "Start der Sicherung $(date)."
+    $source/script.sh SAVE_BACKUP stop
+    echo "Start of backup $(date)."
 
-    borg create --compression $kompression --exclude-caches --one-file-system -v --stats --progress \
-                $repopfad::'{hostname}-{now:%Y-%m-%d-%H%M%S}' $sicherung
+    borg create --compression $compression --exclude-caches --one-file-system -v --stats --progress \
+                $repopfad::'{hostname}-{now:%Y-%m-%d-%H%M%S}' $source
 
-    echo "Ende der Sicherung $(date). Dauer: $SECONDS Sekunden"
-    /mnt/data/homeserver/script.sh SAVE_BACKUP start
+    echo "End of backup $(date). Duration: $SECONDS seconds"
+    $source/script.sh SAVE_BACKUP start
 
-    # prune archives
     borg prune -v --list $repopfad --prefix '{hostname}-' $pruning
+
 else
     echo "!!! DIRECTORIES NOT MOUNTED !!!"
 fi
